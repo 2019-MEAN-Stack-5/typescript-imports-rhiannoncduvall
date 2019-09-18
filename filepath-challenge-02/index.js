f9c1042c84c7617100f9fd8cc08104ebd7b31e2a
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2019-09-17 21:57:01 -0700

    filepath challenge 02 passed tests

--- filepath-challenge-02/index.js
+++ filepath-challenge-02/index.js
@@ -1,10 +1,9 @@
+"use strict";
 /* import the double function from double.ts
    now invoke the function with 512 as an input. Store the output in an exported variable named result.
    confirm completion by running tests for this directory.
 */
-
-import { double } from "./js/functions/math/double";
-
-let result = double(512);
-
-export { result };
+exports.__esModule = true;
+var double_1 = require("./js/functions/math/double");
+var result = double_1.double(512);
+exports.result = result;
